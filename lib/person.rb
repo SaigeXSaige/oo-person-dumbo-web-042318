2465adba9c2744b8dacc0929e7101027d2f58db0
SaigeXSaige <saige.leslie@flatironschool.com>
2018-04-25 15:42:00 -0400

    added a class with two methods (initialize and bank_account) to person.rb

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,27 @@
-# your code goes here
+require 'pry'
+
+class Person 
+
+    @@name = []
+  
+    attr_writer :bank_account
+    attr_reader :name 
+
+    def initialize(name, bank_account = 25)
+        @@name << @name = name 
+        @bank_account = bank_account
+    end
+
+    def bank_account
+        puts "$#{@bank_account}"
+    end
+
+    
+
+    #def name
+      #  @name
+   # end
+end
+
+binding.pry
+true
